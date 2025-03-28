bb2777a8f12e3fb067491c12d85b592ae1d82953
TARUMT <phuah504@gmail.com>
2025-03-28 19:01:49 +0800

    .

---
+++ lib/main.dart
@@ -0,0 +1,206 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:shared_preferences/shared_preferences.dart';
+
+//TODO 2: - Insert TextEditingController
+final _nameTextEditingController = TextEditingController();
+final _emailTextEditingController = TextEditingController();
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SharedPreferenceDemo(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      // appBar: AppBar(
+      //   // TRY THIS: Try changing the color here to a specific color (to
+      //   // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+      //   // change color while the other colors stay the same.
+      //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      //   // Here we take the value from the MyHomePage object that was created by
+      //   // the App.build method, and use it to set our appbar title.
+      //   title: Text(widget.title),
+      // ),
+      // body: Center(
+      //  child: Column(
+      //    children: [
+      //      TextField(
+      //        controller: _nameTextEditingController,
+      //        keyboardType: TextInputType.name,
+      //        decoration: const InputDecoration(
+      //          labelText: 'Name',
+      //        ),
+      //      ),
+      //      const SizedBox(),
+      //      TextField(
+      //        controller: _emailTextEditingController,
+      //        keyboardType: TextInputType.emailAddress,
+      //        decoration: const InputDecoration(
+      //          labelText: 'Email',
+      //        ),
+      //      ),
+      //      const Expanded(child: SizedBox(),),
+      //      ElevatedButton(
+      //       onPressed: (){
+      //         //_updateProfile();
+      //         ScaffoldMessenger.of(context).showSnackBar(
+      //           const SnackBar(
+      //             content: Text('Profile info saved.'),
+      //           ),
+      //         );
+      //       },
+      //       child: const Text('Save')
+      //       ),
+      //    ],
+      //  ),
+      // ) // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class SharedPreferenceDemo extends StatefulWidget {
+  const SharedPreferenceDemo({super.key});
+
+  @override
+  State<SharedPreferenceDemo> createState() => _SharedPreferenceDemoState();
+}
+
+class _SharedPreferenceDemoState extends State<SharedPreferenceDemo> {
+
+  Future<void>_loadProfile() async{
+    final pref = await SharedPreferences.getInstance();
+    setState(() {
+      _nameTextEditingController.text = pref.getString("name") ?? "";
+      _emailTextEditingController.text = pref.getString("email") ?? "";
+    });
+  }
+
+  Future<void>_updateProfile() async{
+    final pref = await SharedPreferences.getInstance();
+    setState(() {
+      pref.setString('name', _nameTextEditingController.text);
+      pref.setString('email', _emailTextEditingController.text);
+    });
+  }
+
+  @override
+  void initState(){
+    _loadProfile();
+    super.initState();
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+    _nameTextEditingController.dispose();
+    _emailTextEditingController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // TRY THIS: Try changing the color here to a specific color (to
+          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+          // change color while the other colors stay the same.
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text('Shared Preferences'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              TextField(
+                controller: _nameTextEditingController,
+                keyboardType: TextInputType.name,
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                ),
+              ),
+              const SizedBox(),
+              TextField(
+                controller: _emailTextEditingController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                ),
+              ),
+              const Expanded(child: SizedBox(),),
+              ElevatedButton(
+                  onPressed: (){
+                    _updateProfile();
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Profile info saved.'),
+                      ),
+                    );
+                  },
+                  child: const Text('Save')
+              ),
+            ],
+          ),
+        ) // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
